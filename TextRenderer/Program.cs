41de5ae847343aa3640f4f6333b5ff7e67f78836
wxhami <d18102002@mail.ru>
2022-11-24 21:01:51 +0300

    add GetFile

--- TextRenderer/Program.cs
+++ TextRenderer/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿Console.WriteLine("specify the path to the directory");
+string Path = Console.ReadLine();
+FileInfo file = GetFile(Path);
+var text = File.ReadAllText(file.FullName);
+Console.WriteLine(text);
+
+Console.ReadLine();
+
+ FileInfo GetFile(string path)
+{
+    Path = path;
+    DirectoryInfo directoryInfo = new DirectoryInfo(path);
+    FileInfo fileWithMaxDate = null;
+
+    if (Directory.Exists(path))
+    {
+        var files = directoryInfo.GetFiles("*.txt");
+        DateTime maxdate = DateTime.MinValue;
+        
+        
+        foreach (var fileInfo in files)
+        {
+            if (maxdate < fileInfo.CreationTime)
+            {
+                maxdate = fileInfo.CreationTime;
+                fileWithMaxDate = fileInfo;
+            }
+        }
+     
+
+    }
+
+    return fileWithMaxDate;
+}
